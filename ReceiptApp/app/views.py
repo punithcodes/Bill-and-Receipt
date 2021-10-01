38775535988acbed2cc3b2942ca8902c61cc86d5
Puneeth M S <68369945+punithcodes@users.noreply.github.com>
2021-10-01 09:38:59 +0530

    Update views.py

--- ReceiptApp/app/views.py
+++ ReceiptApp/app/views.py
@@ -4,18 +4,23 @@ from django.shortcuts import render
 from .forms import ItemForm
 from datetime import *
 
+# Here I have created date objects which is required for recording the time and date of the request sent by the client to API. 
 today_date = date.today()
 today_time = datetime.now().strftime("%H:%M:%S")
 
 
+# This function is responsible for displaying homepage for the user
 def home(request):
     form = ItemForm()
     return render(request, 'homepage.html', {'form': form})
 
 
+# This is the url endpoint which accepts request from client.
 URL = "https://antstackbillingapi.herokuapp.com/billing/"
 
 
+'''This function is responsible for getting the user's data from Itemform, convert it to JSON format and send it to Billing API, 
+getting the response and displaying it to the user'''
 def receipt(request):
     if request.method == 'POST':
         category = request.POST['category']
