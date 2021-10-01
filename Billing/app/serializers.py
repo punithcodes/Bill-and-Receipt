627b679ea4f6673a7b528281049df238f3d12b0c
Puneeth M S <68369945+punithcodes@users.noreply.github.com>
2021-10-01 09:18:00 +0530

    Update serializers.py

--- Billing/app/serializers.py
+++ Billing/app/serializers.py
@@ -1,7 +1,7 @@
 from .models import BillingModel
 from rest_framework import serializers
 
-
+# Here I have inherited ModelSerializer and defined below class which is responsible for serialization and de-serialization.
 class BillingSerializer(serializers.ModelSerializer):
     class Meta:
         model = BillingModel
